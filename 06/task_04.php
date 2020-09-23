c4b1f31684507a2a17766491e1c2a812170a5dfb
Andrei <70196299+AndreiBabinets@users.noreply.github.com>
2020-09-23 12:59:33 +0300

    Create task_04.php

---
+++ 06/task_04.php
@@ -0,0 +1,13 @@
+<?php
+$digits = array(2, -10, -2, 4, 5, 1, 6, 200, 1.6, 95);
+deleteNegtives($digits); 
+
+print_r($digits);
+
+function deleteNegtives(&$digits){
+    foreach($digits as $key=>$value){
+        if ($value<0) unset($digits[$key]);
+    }
+}
+
+?> 
